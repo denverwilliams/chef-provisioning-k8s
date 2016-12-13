01dc582a5c22f5c6615b03247a372d8723f7b840
DLX <denver@ii.org.nz>
2016-12-13 11:00:05 +0000

    Add Arm Kubectl

--- provision/attributes/default.rb
+++ provision/attributes/default.rb
@@ -4,7 +4,9 @@ node.normal['provision']['k8s']['service_ip_range']='10.3.0.0/24'
 node.normal['provision']['k8s']['pod_network']='10.2.0.0/16'
 
 node.normal['provision']['k8s']['kubectl']['version'] = '1.4.3'
-node.normal['provision']['k8s']['kubectl']['sha'] = '573bc0f5a7fe95b166c8e2239f70a7c9a56c2805afd0d2b99643dcb07fbebdf1'
-node.normal['provision']['k8s']['kubectl']['arch'] = 'amd64'
+node.normal['provision']['k8s']['kubectl']['sha'] = 'a23e0174e526dc6c132c5a1fa150b523dc3f37b1fd653b2f5370fbb9bc54b5c9'
+node.normal['provision']['k8s']['kubectl']['arch'] = 'arm'
+#arm
+#a23e0174e526dc6c132c5a1fa150b523dc3f37b1fd653b2f5370fbb9bc54b5c9
 #arm64
-
+#b57da43da3f275e62a70ce2ac2ed7c2472263d6db0cfeec72a77cad8a5aa22e1
